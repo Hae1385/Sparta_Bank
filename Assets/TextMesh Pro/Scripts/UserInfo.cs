1ff8e4be6e2317cb3903be9ddd0af379b60fdd83
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-04 13:44:21 +0900

    Add : Title
    타이틀 화면 제작

---
+++ Assets/TextMesh Pro/Scripts/UserInfo.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UserInfo : MonoBehaviour
+{
+    public TextMeshProUGUI UserName;
+    public TextMeshProUGUI Banlance;
+    public TextMeshProUGUI Cash;
+    public string userName;
+    public int banlance;
+    public int cash;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        UpdateCash();
+    }
+
+    public void UpdateCash()
+    {
+        UserName.text = userName;
+        Banlance.text = string.Format("{0:N0}", banlance);
+        Cash.text = string.Format("{0:N0}", cash);
+    }
+}
